bbf36ff97c31a553b1fa47426e03962ada10aeeb
fiml1205 <tuanleanh120599@gmail.com>
2024-10-17 15:10:52 +0700

    push code

---
+++ tailwind.config.ts
@@ -0,0 +1,90 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+	darkMode: ["class"],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['var(--font-roboto)'],
+			},
+			colors: {
+				'default-color': '#FF3366',
+				'hover-default-color': 'rgb(178, 35, 71)',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				chart: {
+					'1': 'hsl(var(--chart-1))',
+					'2': 'hsl(var(--chart-2))',
+					'3': 'hsl(var(--chart-3))',
+					'4': 'hsl(var(--chart-4))',
+					'5': 'hsl(var(--chart-5))'
+				}
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			height: {
+				'450px': '450px',
+				'400px': '400px',
+				'14px': '14px',
+			},
+			width: {
+				'600px': '600px',
+				'1200px': '1200px',
+				'14px': '14px',
+				'calc(100%-56px)': 'calc(100% - 56px)'
+			},
+			fontSize: {
+				'13px': '13px',
+				'8px': '8px'
+			},
+			maxWidth: {
+				'400px': '400px',
+				'1200px': '1200px',
+			},
+		}
+	},
+	plugins: [
+		require("tailwindcss-animate"),
+		require('@tailwindcss/line-clamp'),
+	],
+};
+export default config;
